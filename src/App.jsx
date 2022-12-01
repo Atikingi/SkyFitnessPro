7271bc851782cba241ed60ae96a4e88c94989787
DESKTOP-LQN6SK2\vikta <12.black.staff.12@gmail.com>
2022-12-02 00:38:24 +0300

    prof__card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ProfCard } from './pages/prof-card/prof-card';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ProfCard />
+    </div>
+  );
+}
+
+export default App;
