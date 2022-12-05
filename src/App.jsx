eda693386db51490aed94da86b3efe830ce803b8
DESKTOP-LQN6SK2\vikta <12.black.staff.12@gmail.com>
2022-12-05 17:22:14 +0300

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-// import { MainFitness } from './components/MainSkyFitnessPro/MainSkyFitnessPro';
-import { CourseDescription } from './components/CourseDescription/CourseDescription';
+import { MainFitness } from './components/MainSkyFitnessPro/MainSkyFitnessPro';
 
 function App() {
-  // return <MainFitness />
-  return <CourseDescription />;
+  return <MainFitness />;
 }
 
 export default App;
