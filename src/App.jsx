8faca73093b0c936af95da73ab3e87c8303a639a
lokkib <lokkichebrikov@yandex.ru>
2022-12-08 16:19:40 +0300

    Layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { MainFitness } from './components/MainSkyFitnessPro/MainSkyFitnessPro';
+import Layout from './layouts/Layout';
 
 function App() {
   return (
     <>
-      {/* <UserProfile /> */}
-      <MainFitnessPage />;
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* тут Routes с путями на страницы Ильи */}
+        </Route>
+      </Routes>
     </>
   );
 }
