94952fbe69cd192c449d364e73553b4167e77ed0
DESKTOP-LQN6SK2\vikta <12.black.staff.12@gmail.com>
2022-12-11 11:31:24 +0300

    removeThunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 export const store = configureStore({
-  reducer: {},
-  middleware: [thunk]
+  reducer: {}
 });
