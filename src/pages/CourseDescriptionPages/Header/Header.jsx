8ef0794543c63e98946c22e60a0ef53427eda256
17u5h <102058870+17u5h@users.noreply.github.com>
2022-12-13 21:25:26 +0700

    ProtectedRoute

--- src/pages/CourseDescriptionPages/Header/Header.jsx
+++ src/pages/CourseDescriptionPages/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '../../../components/Logo/Logo';
 export const Header = () => {
   return (
     <div className={styles.logo}>
-      <Logo />
+      <Logo color="#000" />
     </div>
   );
 };
