b9516b787892bc98ca0ba8430d49716168d9144a
17u5h <102058870+17u5h@users.noreply.github.com>
2022-12-21 21:19:04 +0700

    button scroll to top

--- src/pages/MainSkyFitnessProPage/Footer/Footer.jsx
+++ src/pages/MainSkyFitnessProPage/Footer/Footer.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styles from './style.module.css';
 import { ButtonGreen } from '../../../components/ButtonGreen/ButtonGreen';
 
-export const Footer = () => {
+export const Footer = (props) => {
   const btnText = 'Наверх ↑';
   return (
     <footer className={styles.footer}>
-      <ButtonGreen className={styles.button} btnText={btnText}></ButtonGreen>
+      <ButtonGreen
+        className={styles.button}
+        btnText={btnText}
+        onClick={props.onClick}
+      ></ButtonGreen>
     </footer>
   );
 };
