7c3b969dbcefaa2e93f29d6754f0a20b88b3494a
DESKTOP-LQN6SK2\vikta <12.black.staff.12@gmail.com>
2022-12-24 20:09:40 +0300

    NIKITA KOSYAKNUL, VERNUL NAZAD

--- src/pages/CourseProgressPage/CourseVideo/CourseVideo.jsx
+++ src/pages/CourseProgressPage/CourseVideo/CourseVideo.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import styles from './style.module.css';
-import PlayButtonIcon from '../../../components/Icons/PlayButtonIcon';
-import { useState } from 'react';
 
 const CourseVideo = (workoutVideo) => {
   const VIDEO_ID = workoutVideo.workoutVideo;
-  const [showVideo, setShowVideo] = useState(false);
+  const videoUrl = `http://www.youtube.com/embed/${VIDEO_ID}`;
+  console.log(videoUrl);
 
-  const ClickShowVideo = (event) => {
-    event.preventDefault();
-    setShowVideo((prevState) => !prevState);
-    console.log(showVideo);
-  };
   return (
     <div className={styles.container}>
-      {/* <div className={styles.imageWrapper}>
-        <img src="/img/video-preview.jpg" width="1160" height="639" alt="Видео тренировки" />
-        <div className={styles.playButton} onClick={ClickShowVideo}>
-          <PlayButtonIcon />
-        </div>
-      </div>
-      {showVideo && ( */}
       <iframe
         width="1160"
         height="639"
-        src={`http://www.youtube.com/embed/${VIDEO_ID}?modestbranding=1&autoplay=0&rel=0&color=white;`}
+        src={`${videoUrl}?modestbranding=1&autoplay=0&rel=0&color=white;`}
       ></iframe>
-      {/* )} */}
     </div>
   );
 };
